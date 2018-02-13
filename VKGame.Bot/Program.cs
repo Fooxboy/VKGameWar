f9f4dec9acb3081e005a35c960e3897385855b26
Fooxboy <Fooxboy@yandex.ru>
2018-02-13 19:24:16 +0300

    Добавьте файлы проекта.

---
+++ VKGame.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VKGame.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
