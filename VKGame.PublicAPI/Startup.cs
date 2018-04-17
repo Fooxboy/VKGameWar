a635fc18457c3b3d7e798165d8f73129bf426f92
Славик Лис <Fooxboy@yandex.ru>
2018-04-17 23:16:19 +0300

    beta 1.5 Удаление PublicAPI

--- VKGame.PublicAPI/Startup.cs
+++ VKGame.PublicAPI/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Http;
 
 namespace VKGame.PublicAPI
 {
@@ -33,8 +34,6 @@ namespace VKGame.PublicAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseMvc();
         }
     }
 }
